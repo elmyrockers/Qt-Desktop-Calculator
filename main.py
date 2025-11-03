6376a2ca9e30921f7feb7c28318c38ec6e761811
elmyrockers <elmyrockers@gmail.com>
2025-11-03 10:15:30 +0800

    Update entry point file

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import sys
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QFontDatabase
+from qt_material import apply_stylesheet
 from calculator import Calculator  # import Calculator class
 
 def main():
     app = QApplication(sys.argv)
+
+    # Apply a dark theme
+    apply_stylesheet(app, theme='dark_teal.xml')
+
     window = Calculator()
     window.show()
     sys.exit(app.exec())
