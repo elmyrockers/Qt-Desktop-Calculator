15032d31b5dc9da0c52255bb61e3d5864e6fde22
elmyrockers <elmyrockers@gmail.com>
2025-11-03 09:07:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-
-from PySide6.QtWidgets import QApplication, QMainWindow
 import sys
-from calculator_ui import Ui_MainWindow  # the generated file
+from PySide6.QtWidgets import QApplication
+from calculator import Calculator  # import Calculator class
 
-class Calculator(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+def main():
+    app = QApplication(sys.argv)
+    window = Calculator()
+    window.show()
+    sys.exit(app.exec())
 
+if __name__ == "__main__":
+    main()
 
-        
-
-app = QApplication(sys.argv)
-window = Calculator()
-window.show()
-sys.exit(app.exec())
